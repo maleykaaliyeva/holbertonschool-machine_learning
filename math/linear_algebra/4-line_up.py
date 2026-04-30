4c7c4c0f2eb01a5bf8d33ec5b4d236c854ff2d14
maleykaaliyeva <meleykeliyeva16@gmail.com>
2026-04-30 11:02:34 +0000

    4. Line Up

---
+++ math/linear_algebra/4-line_up.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Defines a function that adds two arrays element-wise
+"""
+
+
+def add_arrays(arr1, arr2):
+    """
+    Adds two arrays element-wise and returns a new list
+    Returns None if arrays are not the same shape
+    """
+    if len(arr1) != len(arr2):
+        return None
+
+    return [arr1[i] + arr2[i] for i in range(len(arr1))]
