ea33c97f13bbce075833edb80ad8bd95a5a1357f
maleykaaliyeva <meleykeliyeva16@gmail.com>
2026-04-30 11:07:45 +0000

    6. Howdy Partner

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Defines a function that concatenates two arrays
+"""
+
+
+def cat_arrays(arr1, arr2):
+    """
+    Concatenates two lists and returns a new list
+    """
+    return arr1 + arr2
