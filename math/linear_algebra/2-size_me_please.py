141b2d92bdee144f806f7d08297c4e0fbbcc965e
maleykaaliyeva <meleykeliyeva16@gmail.com>
2026-04-30 09:54:33 +0000

    2. Size Me Please

---
+++ math/linear_algebra/2-size_me_please.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Defines a function that calculates the shape of a matrix
+"""
+
+
+def matrix_shape(matrix):
+    """
+    Calculates the shape of a matrix and returns it as a list of integers
+    """
+    shape = []
+    while isinstance(matrix, list):
+        shape.append(len(matrix))
+        matrix = matrix[0]
+    return shape
