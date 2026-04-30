475bf660b2166afe455b945c0abaa4272a2b7513
maleykaaliyeva <meleykeliyeva16@gmail.com>
2026-04-30 12:04:50 +0000

    14. Saddle Up

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Performs matrix multiplication using numpy
+"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """
+    Returns the matrix multiplication of mat1 and mat2
+    """
+    return np.matmul(mat1, mat2)
