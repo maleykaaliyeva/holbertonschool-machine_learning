5e6bb03d888efc3a9ab12132eae2cac80182de18
maleykaaliyeva <meleykeliyeva16@gmail.com>
2026-04-30 11:24:27 +0000

    7. Gettin’ Cozy

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -1,22 +1,21 @@
 #!/usr/bin/env python3
-""" Simple 2D concatenation """
+"""
+Defines a function that concatenates two matrices
+"""
 
 
 def cat_matrices2D(mat1, mat2, axis=0):
-    """ Concatenates matrices based on axis """
-    
+    """
+    Concatenates two 2D matrices along a specific axis
+    """
     if axis == 0:
-        # Check if widths match
         if len(mat1[0]) != len(mat2[0]):
             return None
-        # Just glue the lists of rows together
-        return mat1 + mat2
+        return [row.copy() for row in mat1] + [row.copy() for row in mat2]
 
     if axis == 1:
-        # Check if heights match
         if len(mat1) != len(mat2):
             return None
-        # Glue each individual row side-by-side
         return [mat1[i] + mat2[i] for i in range(len(mat1))]
 
     return None
