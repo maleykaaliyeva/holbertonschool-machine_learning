19400d3d2ddac45d59c52942f43715762f168153
maleykaaliyeva <meleykeliyeva16@gmail.com>
2026-05-14 19:45:21 +0000

    6. Initialize Normal

---
+++ math/probability/normal.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+Module for Normal distribution
+"""
+
+
+class Normal:
+    """
+    Class that represents a normal distribution
+    """
+
+    def __init__(self, data=None, mean=0., stddev=1.):
+        """
+        Initialize Normal distribution
+        """
+        if data is None:
+            if stddev <= 0:
+                raise ValueError("stddev must be a positive value")
+            self.mean = float(mean)
+            self.stddev = float(stddev)
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+
+            # Calculate mean
+            self.mean = float(sum(data) / len(data))
+
+            # Calculate standard deviation
+            sum_diff_sq = sum([(x - self.mean) ** 2 for x in data])
+            variance = sum_diff_sq / len(data)
+            self.stddev = float(variance ** 0.5)
