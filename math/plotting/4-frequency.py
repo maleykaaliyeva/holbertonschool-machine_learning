41ac7d2e7726b8fc6e583a01812ccfc52a7dc0dd
maleykaaliyeva <meleykeliyeva16@gmail.com>
2026-05-28 12:17:00 +0000

    Frequency

---
+++ math/plotting/4-frequency.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+This module defines the frequency function.
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def frequency():
+    """
+    Plots a histogram of student grades.
+    """
+    np.random.seed(5)
+    student_grades = np.random.normal(68, 15, 50)
+    plt.figure(figsize=(6.4, 4.8))
+
+    bins = np.arange(0, 101, 10)
+
+    plt.hist(student_grades, bins=bins, edgecolor="black")
+    plt.xlabel("Grades")
+    plt.ylabel("Number of Students")
+    plt.title("Project A")
+    plt.xlim(0, 100)
+    plt.xticks(bins)
+    plt.show()
