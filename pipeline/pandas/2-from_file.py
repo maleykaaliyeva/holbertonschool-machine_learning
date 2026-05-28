ca8c9dfbccd1a50d361c53278f9ed721c74fd9b0
maleykaaliyeva <meleykeliyeva16@gmail.com>
2026-05-28 11:49:22 +0000

    From File

---
+++ pipeline/pandas/2-from_file.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Loads data from a file as a pandas DataFrame.
+"""
+
+import pandas as pd
+
+
+def from_file(filename, delimiter):
+    """
+    Loads data from a file as a pd.DataFrame.
+
+    Args:
+        filename: The file to load from.
+        delimiter: The column separator.
+
+    Returns:
+        The loaded pd.DataFrame.
+    """
+    return pd.read_csv(filename, delimiter=delimiter)
