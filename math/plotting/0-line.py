f19f5471f3d12f78b6530f15d167fc04800a36ca
maleykaaliyeva <meleykeliyeva16@gmail.com>
2026-05-28 12:12:40 +0000

    Line Graph

---
+++ math/plotting/0-line.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Plots y as a solid red line graph.
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def line():
+    """
+    Plots y = x^3 as a solid red line.
+    """
+    y = np.arange(0, 11) ** 3
+    plt.figure(figsize=(6.4, 4.8))
+
+    plt.plot(y, "r-")
+    plt.xlim(0, 10)
+    plt.show()
---
+++ math/plotting/README.md
@@ -0,0 +1,1 @@
+This directory contains tasks for learning how to create plots and visualizations in Python using Matplotlib.
