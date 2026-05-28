21f17b20895be0b27cc623a10376d755959c107c
maleykaaliyeva <meleykeliyeva16@gmail.com>
2026-05-28 12:22:30 +0000

    Fix frequency plot

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """This module defines frequency function."""
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -9,11 +10,10 @@ def frequency():
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
+
+    plt.hist(student_grades, bins=np.arange(0, 101, 10), edgecolor="black")
     plt.xlabel("Grades")
     plt.ylabel("Number of Students")
     plt.title("Project A")
     plt.axis([0, 100, 0, 30])
-    bins = np.arange(0, 101, 10)
-    plt.hist(student_grades, bins=bins, edgecolor="k")
-    plt.xticks(np.arange(0, 101, 10))
-    plt.show() 
+    plt.show()
