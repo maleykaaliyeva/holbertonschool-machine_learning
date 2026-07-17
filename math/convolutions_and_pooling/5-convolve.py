77987a4b169afadf69e07609beb95926d73a2d6e
maleykaaliyeva <meleykeliyeva16@gmail.com>
2026-07-17 17:36:24 +0000

    5. Multiple Kernels

---
+++ math/convolutions_and_pooling/5-convolve.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""Multiple Kernels."""
+import numpy as np
+
+
+def convolve(images, kernels, padding='same', stride=(1, 1)):
+    """Return a numpy.ndarray containing the convolved images"""
+    m, h, w, c = images.shape
+    kh, kw, c, nc = kernels.shape
+    sh, sw = stride
+    if padding == 'same':
+        ph = int(np.ceil(((h - 1) * sh + kh - h) / 2))
+        pw = int(np.ceil(((w - 1) * sw + kw - w) / 2))
+
+    if padding == 'valid':
+        ph = 0
+        pw = 0
+
+    if isinstance(padding, tuple):
+        ph, pw = padding
+
+    new_w = (w + 2 * pw - kw) // sw + 1
+    new_h = (h + 2 * ph - kh) // sh + 1
+    convolved = np.zeros((m, new_h, new_w, nc))
+    padded_images = np.pad(images,
+                           pad_width=((0, 0), (ph, ph),
+                                      (pw, pw), (0, 0)),
+                           mode='constant', constant_values=0)
+    for k in range(nc):
+        for i in range(new_h):
+            for j in range(new_w):
+                convolved[:, i, j, k] = np.sum(
+                    padded_images[:, i * sh:i * sh + kh,
+                                  j * sw:j * sw + kw, :] * kernels[:, :, :, k],
+                    axis=(1, 2, 3))
+    return convolved
