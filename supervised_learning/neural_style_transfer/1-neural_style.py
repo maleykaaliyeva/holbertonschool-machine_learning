4e61910f2d8bd31084684c62c3b8efc04dc6018c
maleykaaliyeva <meleykeliyeva16@gmail.com>
2026-08-15 17:50:16 +0000

    1. Load the Model

--- supervised_learning/neural_style_transfer/1-neural_style.py
+++ supervised_learning/neural_style_transfer/1-neural_style.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Defines the Neural Style Transfer class."""
+"""Neural Style Transfer."""
 
 import numpy as np
 import tensorflow as tf
@@ -15,6 +15,7 @@ class NST:
         'block4_conv1',
         'block5_conv1'
     ]
+
     content_layer = 'block5_conv2'
 
     def __init__(self, style_image, content_image, alpha=1e4, beta=1):
@@ -23,27 +24,27 @@ class NST:
                 or len(style_image.shape) != 3 \
                 or style_image.shape[2] != 3:
             raise TypeError(
-                "style_image must be a numpy.ndarray with shape (h, w, 3)"
+                'style_image must be a numpy.ndarray with shape (h, w, 3)'
             )
 
         if not isinstance(content_image, np.ndarray) \
                 or len(content_image.shape) != 3 \
                 or content_image.shape[2] != 3:
             raise TypeError(
-                "content_image must be a numpy.ndarray with shape (h, w, 3)"
+                'content_image must be a numpy.ndarray with shape (h, w, 3)'
             )
 
         if not isinstance(alpha, (int, float)) or alpha < 0:
-            raise TypeError("alpha must be a non-negative number")
+            raise TypeError('alpha must be a non-negative number')
 
         if not isinstance(beta, (int, float)) or beta < 0:
-            raise TypeError("beta must be a non-negative number")
+            raise TypeError('beta must be a non-negative number')
 
         self.style_image = self.scale_image(style_image)
         self.content_image = self.scale_image(content_image)
         self.alpha = alpha
         self.beta = beta
-        self.load_model()
+        self.model = self.load_model()
 
     @staticmethod
     def scale_image(image):
@@ -52,37 +53,31 @@ class NST:
                 or len(image.shape) != 3 \
                 or image.shape[2] != 3:
             raise TypeError(
-                "image must be a numpy.ndarray with shape (h, w, 3)"
+                'image must be a numpy.ndarray with shape (h, w, 3)'
             )
 
+        height, width, _ = image.shape
+
+        if height > width:
+            new_height = 512
+            new_width = int(width * 512 / height)
+        else:
+            new_width = 512
+            new_height = int(height * 512 / width)
+
         image = tf.convert_to_tensor(image, dtype=tf.float32)
-
-        height = tf.shape(image)[0]
-        width = tf.shape(image)[1]
-
-        scale = 512 / tf.cast(tf.maximum(height, width), tf.float32)
-
-        new_height = tf.cast(
-            tf.cast(height, tf.float32) * scale,
-            tf.int32
-        )
-        new_width = tf.cast(
-            tf.cast(width, tf.float32) * scale,
-            tf.int32
-        )
-
+        image = tf.expand_dims(image, axis=0)
         image = tf.image.resize(
             image,
             (new_height, new_width),
             method='bicubic'
         )
+        image = image / 255.0
 
-        image = tf.clip_by_value(image / 255.0, 0.0, 1.0)
-
-        return tf.expand_dims(image, axis=0)
+        return image
 
     def load_model(self):
-        """Load VGG19 and create the NST model."""
+        """Load the VGG19 model used for neural style transfer."""
         vgg = tf.keras.applications.VGG19(
             include_top=False,
             weights='imagenet'
@@ -90,34 +85,24 @@ class NST:
 
         vgg.trainable = False
 
-        def replace_pooling(layer):
-            """Replace max pooling layers with average pooling layers."""
+        for layer in vgg.layers:
             if isinstance(layer, tf.keras.layers.MaxPooling2D):
-                return tf.keras.layers.AveragePooling2D(
+                layer = tf.keras.layers.AveragePooling2D(
                     pool_size=layer.pool_size,
                     strides=layer.strides,
                     padding=layer.padding,
                     name=layer.name
                 )
-            return layer
-
-        model = tf.keras.models.clone_model(
-            vgg,
-            clone_function=replace_pooling
-        )
-
-        model.set_weights(vgg.get_weights())
-        model.trainable = False
 
         outputs = [
-            model.get_layer(layer_name).output
+            vgg.get_layer(layer_name).output
             for layer_name in self.style_layers
         ]
-        outputs.append(model.get_layer(self.content_layer).output)
+        outputs.append(vgg.get_layer(self.content_layer).output)
 
-        self.model = tf.keras.Model(
-            inputs=model.input,
+        model = tf.keras.Model(
+            inputs=vgg.input,
             outputs=outputs
         )
 
-        self.model.trainable = False
+        return model
