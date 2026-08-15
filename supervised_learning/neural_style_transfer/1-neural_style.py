dc1a300ef673ca00fd8684485f1a3e45a81e935d
maleykaaliyeva <meleykeliyeva16@gmail.com>
2026-08-15 17:56:31 +0000

    1. Load the Model

--- supervised_learning/neural_style_transfer/1-neural_style.py
+++ supervised_learning/neural_style_transfer/1-neural_style.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-"""Neural Style Transfer."""
+"""Module for Neural Style Transfer."""
 
 import numpy as np
 import tensorflow as tf
 
 
 class NST:
-    """Neural Style Transfer class."""
+    """Class that implements Neural Style Transfer."""
 
     style_layers = [
         'block1_conv1',
@@ -15,87 +15,76 @@ class NST:
         'block4_conv1',
         'block5_conv1'
     ]
-
-    content_layer = 'block5_conv2'
+    content_layer = 'block4_conv2'
 
     def __init__(self, style_image, content_image, alpha=1e4, beta=1):
-        """Initialize the NST instance."""
-        if not isinstance(style_image, np.ndarray) \
-                or len(style_image.shape) != 3 \
-                or style_image.shape[2] != 3:
-            raise TypeError(
-                'style_image must be a numpy.ndarray with shape (h, w, 3)'
-            )
+        """Class constructor for NST.
 
-        if not isinstance(content_image, np.ndarray) \
-                or len(content_image.shape) != 3 \
-                or content_image.shape[2] != 3:
+        Args:
+            style_image (np.ndarray): Image used as a style reference.
+            content_image (np.ndarray): Image used as a content reference.
+            alpha (float): Weight for content cost.
+            beta (float): Weight for style cost.
+        """
+        if not isinstance(style_image, np.ndarray) or \
+           style_image.shape[-1] != 3 or len(style_image.shape) != 3:
             raise TypeError(
-                'content_image must be a numpy.ndarray with shape (h, w, 3)'
+                "style_image must be a numpy.ndarray with shape (h, w, 3)"
             )
-
+        if not isinstance(content_image, np.ndarray) or \
+           content_image.shape[-1] != 3 or len(content_image.shape) != 3:
+            raise TypeError(
+                "content_image must be a numpy.ndarray with shape (h, w, 3)"
+            )
         if not isinstance(alpha, (int, float)) or alpha < 0:
-            raise TypeError('alpha must be a non-negative number')
-
+            raise TypeError("alpha must be a non-negative number")
         if not isinstance(beta, (int, float)) or beta < 0:
-            raise TypeError('beta must be a non-negative number')
+            raise TypeError("beta must be a non-negative number")
 
         self.style_image = self.scale_image(style_image)
         self.content_image = self.scale_image(content_image)
-        self.alpha = alpha
-        self.beta = beta
+        self.alpha = float(alpha)
+        self.beta = float(beta)
         self.model = self.load_model()
 
     @staticmethod
     def scale_image(image):
-        """Scale an image so its largest dimension is 512 pixels."""
-        if not isinstance(image, np.ndarray) \
-                or len(image.shape) != 3 \
-                or image.shape[2] != 3:
+        """Rescales an image such that its pixels are between 0 and 1,
+
+        with a maximum dimension of 512 pixels.
+        """
+        if not isinstance(image, np.ndarray) or \
+           image.shape[-1] != 3 or len(image.shape) != 3:
             raise TypeError(
-                'image must be a numpy.ndarray with shape (h, w, 3)'
+                "image must be a numpy.ndarray with shape (h, w, 3)"
             )
 
-        height, width, _ = image.shape
+        image = tf.cast(image, tf.float32)
+        max_dim = 512
+        shape = tf.cast(tf.shape(image)[:-1], tf.float32)
+        long_dim = max(shape)
+        scale = max_dim / long_dim
 
-        if height > width:
-            new_height = 512
-            new_width = int(width * 512 / height)
-        else:
-            new_width = 512
-            new_height = int(height * 512 / width)
+        new_shape = tf.cast(shape * scale, tf.int32)
 
-        image = tf.convert_to_tensor(image, dtype=tf.float32)
-        image = tf.expand_dims(image, axis=0)
-        image = tf.image.resize(
-            image,
-            (new_height, new_width),
-            method='bicubic'
-        )
-
+        image = tf.image.resize(image, new_shape)
         image = image / 255.0
-
+        image = tf.expand_dims(image, axis=0)
         return image
 
     def load_model(self):
-        """Create and load the VGG19 model for neural style transfer."""
-        vgg = tf.keras.applications.VGG19(
+        """Creates the model used to calculate cost using VGG19 as base."""
+        vgg = tf.keras.applications.vgg19.VGG19(
             include_top=False,
             weights='imagenet'
         )
-
         vgg.trainable = False
 
-        outputs = []
+        style_outputs = [vgg.get_layer(name).output for name in self.style_layers]
+        content_output = vgg.get_layer(self.content_layer).output
+        model_outputs = style_outputs + [content_output]
 
-        for layer_name in self.style_layers + [self.content_layer]:
-            outputs.append(vgg.get_layer(layer_name).output)
-
-        model = tf.keras.Model(
+        return tf.keras.models.Model(
             inputs=vgg.input,
-            outputs=outputs
+            outputs=model_outputs
         )
-
-        model.trainable = False
-
-        return model
