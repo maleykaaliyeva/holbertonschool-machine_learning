8117e7600bf21efbaa4ffcc6fde1e8d0b3fd9f99
maleykaaliyeva <meleykeliyeva16@gmail.com>
2026-08-15 17:54:48 +0000

    1. Load the Model

--- supervised_learning/neural_style_transfer/1-neural_style.py
+++ supervised_learning/neural_style_transfer/1-neural_style.py
@@ -48,7 +48,7 @@ class NST:
 
     @staticmethod
     def scale_image(image):
-        """Scale an image to a maximum dimension of 512 pixels."""
+        """Scale an image so its largest dimension is 512 pixels."""
         if not isinstance(image, np.ndarray) \
                 or len(image.shape) != 3 \
                 or image.shape[2] != 3:
@@ -72,12 +72,13 @@ class NST:
             (new_height, new_width),
             method='bicubic'
         )
+
         image = image / 255.0
 
         return image
 
     def load_model(self):
-        """Load the VGG19 model used for neural style transfer."""
+        """Create and load the VGG19 model for neural style transfer."""
         vgg = tf.keras.applications.VGG19(
             include_top=False,
             weights='imagenet'
@@ -85,32 +86,16 @@ class NST:
 
         vgg.trainable = False
 
-        def clone_layer(layer):
-            """Clone a VGG19 layer, replacing max pooling with average pooling."""
-            if isinstance(layer, tf.keras.layers.MaxPooling2D):
-                return tf.keras.layers.AveragePooling2D(
-                    pool_size=layer.pool_size,
-                    strides=layer.strides,
-                    padding=layer.padding,
-                    name=layer.name
-                )
-            return layer.__class__.from_config(layer.get_config())
+        outputs = []
 
-        model = tf.keras.models.clone_model(
-            vgg,
-            clone_function=clone_layer
-        )
+        for layer_name in self.style_layers + [self.content_layer]:
+            outputs.append(vgg.get_layer(layer_name).output)
 
-        model.set_weights(vgg.get_weights())
-        model.trainable = False
-
-        outputs = [
-            model.get_layer(layer_name).output
-            for layer_name in self.style_layers
-        ]
-        outputs.append(model.get_layer(self.content_layer).output)
-
-        return tf.keras.Model(
-            inputs=model.input,
+        model = tf.keras.Model(
+            inputs=vgg.input,
             outputs=outputs
         )
+
+        model.trainable = False
+
+        return model
