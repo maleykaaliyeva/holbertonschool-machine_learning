041f17de75c7271a771eed090dd944230f5c311d
maleykaaliyeva <meleykeliyeva16@gmail.com>
2026-08-29 19:54:10 +0000

    7. Transformer Decoder Block

---
+++ supervised_learning/attention/8-transformer_decoder_block.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+"""
+Transformer Decoder Block
+"""
+
+import tensorflow as tf
+MultiHeadAttention = __import__('6-multihead_attention').MultiHeadAttention
+
+
+class DecoderBlock(tf.keras.layers.Layer):
+    """
+    Decoder block for a Transformer
+    """
+
+    def __init__(self, dm, h, hidden, drop_rate=0.1):
+        """
+        Class constructor
+
+        Args:
+            dm (int): dimensionality of the model
+            h (int): number of attention heads
+            hidden (int): number of hidden units in the feed-forward network
+            drop_rate (float): dropout rate
+        """
+        super(DecoderBlock, self).__init__()
+
+        self.mha1 = MultiHeadAttention(dm, h)
+        self.mha2 = MultiHeadAttention(dm, h)
+
+        self.dense_hidden = tf.keras.layers.Dense(
+            hidden, activation='relu'
+        )
+        self.dense_output = tf.keras.layers.Dense(dm)
+
+        self.layernorm1 = tf.keras.layers.LayerNormalization(
+            epsilon=1e-6
+        )
+        self.layernorm2 = tf.keras.layers.LayerNormalization(
+            epsilon=1e-6
+        )
+        self.layernorm3 = tf.keras.layers.LayerNormalization(
+            epsilon=1e-6
+        )
+
+        self.dropout1 = tf.keras.layers.Dropout(drop_rate)
+        self.dropout2 = tf.keras.layers.Dropout(drop_rate)
+        self.dropout3 = tf.keras.layers.Dropout(drop_rate)
+
+    def call(self, x, encoder_output, training,
+             look_ahead_mask, padding_mask):
+        """
+        Forward pass for the decoder block
+
+        Args:
+            x (tf.Tensor): (batch, target_seq_len, dm)
+            encoder_output (tf.Tensor): (batch, input_seq_len, dm)
+            training (bool): training mode flag
+            look_ahead_mask: mask for masked self-attention
+            padding_mask: mask for encoder-decoder attention
+
+        Returns:
+            tf.Tensor: (batch, target_seq_len, dm)
+        """
+        attn1, _ = self.mha1(x, x, x, look_ahead_mask)
+        attn1 = self.dropout1(attn1, training=training)
+        out1 = self.layernorm1(x + attn1)
+
+        attn2, _ = self.mha2(
+            out1, encoder_output, encoder_output, padding_mask
+        )
+        attn2 = self.dropout2(attn2, training=training)
+        out2 = self.layernorm2(out1 + attn2)
+
+        ffn_output = self.dense_hidden(out2)
+        ffn_output = self.dense_output(ffn_output)
+        ffn_output = self.dropout3(ffn_output, training=training)
+
+        return self.layernorm3(out2 + ffn_output)
