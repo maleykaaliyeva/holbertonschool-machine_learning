fa515b48e5b7bea00070e86d0a92fd32f23c365e
maleykaaliyeva <meleykeliyeva16@gmail.com>
2026-08-29 19:57:20 +0000

    10. Transformer Network

---
+++ supervised_learning/attention/11-transformer.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+"""Transformer Network Module"""
+import tensorflow as tf
+Encoder = __import__('9-transformer_encoder').Encoder
+Decoder = __import__('10-transformer_decoder').Decoder
+
+
+class Transformer(tf.keras.Model):
+    """
+    Transformer model class that combines encoder and decoder
+    for sequence-to-sequence tasks
+    """
+
+    def __init__(self, N, dm, h, hidden, input_vocab, target_vocab,
+                 max_seq_input, max_seq_target, drop_rate=0.1):
+        """
+        Initialize the Transformer model
+
+        Args:
+            N: the number of blocks in the encoder and decoder
+            dm: the dimensionality of the model
+            h: the number of heads
+            hidden: the number of hidden units in the fully connected layers
+            input_vocab: the size of the input vocabulary
+            target_vocab: the size of the target vocabulary
+            max_seq_input: the maximum sequence length possible for the input
+            max_seq_target: the maximum sequence length possible for target
+            drop_rate: the dropout rate
+        """
+        super(Transformer, self).__init__()
+
+        self.encoder = Encoder(N, dm, h, hidden, input_vocab,
+                               max_seq_input, drop_rate)
+        self.decoder = Decoder(N, dm, h, hidden, target_vocab,
+                               max_seq_target, drop_rate)
+        self.linear = tf.keras.layers.Dense(target_vocab)
+
+    def call(self, inputs, target, training, encoder_mask,
+             look_ahead_mask, decoder_mask):
+        """
+        Forward pass through the transformer
+
+        Args:
+            inputs: tensor of shape (batch, input_seq_len) containing inputs
+            target: tensor of shape (batch, target_seq_len) containing target
+            training: boolean to determine if the model is training
+            encoder_mask: the padding mask to be applied to the encoder
+            look_ahead_mask: the look ahead mask to be applied to the decoder
+            decoder_mask: the padding mask to be applied to the decoder
+
+        Returns:
+            tensor of shape (batch, target_seq_len, target_vocab)
+            containing the transformer output
+        """
+        enc_output = self.encoder(inputs, training, encoder_mask)
+        dec_output = self.decoder(target, enc_output, training,
+                                  look_ahead_mask, decoder_mask)
+
+        final_output = self.linear(dec_output)
+        return final_output
