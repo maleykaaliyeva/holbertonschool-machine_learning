bb9862ab2dd23965c3635ba07073e479ba33c3ab
maleykaaliyeva <meleykeliyeva16@gmail.com>
2026-08-30 19:49:03 +0000

    last

--- reinforcement_learning/q_learning/3-q_learning.py
+++ reinforcement_learning/q_learning/3-q_learning.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
-"""
-Module to perform Q-learning on a Frozen Lake environment
-"""
+"""Performs Q-learning"""
 import numpy as np
 epsilon_greedy = __import__('2-epsilon_greedy').epsilon_greedy
 
@@ -9,48 +7,49 @@ epsilon_greedy = __import__('2-epsilon_greedy').epsilon_greedy
 def train(env, Q, episodes=5000, max_steps=100, alpha=0.1, gamma=0.99,
           epsilon=1, min_epsilon=0.1, epsilon_decay=0.05):
     """
-    Performs Q-learning training
+    Performs Q-learning
 
-    Args:
-        env: the FrozenLakeEnv instance
-        Q: numpy.ndarray containing the Q-table
-        episodes: total number of episodes to train over
-        max_steps: maximum number of steps per episode
-        alpha: learning rate
-        gamma: discount rate
-        epsilon: initial threshold for epsilon greedy
-        min_epsilon: minimum value that epsilon should decay to
-        epsilon_decay: decay rate for updating epsilon between episodes
+    env is the FrozenLakeEnv instance
+    Q is a numpy.ndarray containing the Q-table
+    episodes is the total number of episodes to train over
+    max_steps is the maximum number of steps per episode
+    alpha is the learning rate
+    gamma is the discount rate
+    epsilon is the initial threshold for epsilon greedy
+    min_epsilon is the minimum value that epsilon should decay to
+    epsilon_decay is the decay rate for updating epsilon between episodes
+    When the agent falls in a hole, the reward should be updated to be -1
 
-    Returns:
-        Q, total_rewards
+    Returns: Q, total_rewards
+        Q is the updated Q-table
+        total_rewards is a list containing the rewards per episode
     """
     total_rewards = []
     initial_epsilon = epsilon
 
-    for ep in range(episodes):
+    for episode in range(episodes):
         state, _ = env.reset()
         episode_reward = 0
 
-        for _ in range(max_steps):
+        for step in range(max_steps):
             action = epsilon_greedy(Q, state, epsilon)
-            next_state, reward, terminated, truncated, _ = env.step(action)
+            new_state, reward, terminated, truncated, _ = env.step(action)
 
-            # Update Q-value
-            Q[state, action] = Q[state, action] + alpha * (
-                reward + gamma * np.max(Q[next_state]) - Q[state, action]
-            )
+            if terminated and reward == 0:
+                reward = -1
 
-            state = next_state
+            Q[state, action] = (Q[state, action] + alpha *
+                                 (reward + gamma * np.max(Q[new_state]) -
+                                  Q[state, action]))
+
+            state = new_state
             episode_reward += reward
 
             if terminated or truncated:
                 break
 
+        epsilon = (min_epsilon + (initial_epsilon - min_epsilon) *
+                   np.exp(-epsilon_decay * episode))
         total_rewards.append(episode_reward)
-
-        # Decay epsilon after each episode
-        epsilon = min_epsilon + (initial_epsilon - min_epsilon) * \
-            np.exp(-epsilon_decay * ep)
 
     return Q, total_rewards
