320c7ee28ee4a692e8142b2159d95c65e56d7626
maleykaaliyeva <meleykeliyeva16@gmail.com>
2026-08-30 19:35:53 +0000

    task4

--- reinforcement_learning/q_learning/4-play.py
+++ reinforcement_learning/q_learning/4-play.py
@@ -7,35 +7,28 @@ import numpy as np
 
 def play(env, Q, max_steps=100):
     """
-    Has the trained agent play an episode
+    Has the trained agent play an episode using pure exploitation
 
     Args:
         env: the FrozenLakeEnv instance
         Q: a numpy.ndarray containing the Q-table
-        max_steps: the maximum number of steps in the episode
+        max_steps: maximum number of steps in the episode
 
     Returns:
-        total_reward, rendered_outputs
+        total_rewards, rendered_outputs
     """
-    # Reset the environment and capture the initial board state
     state, _ = env.reset()
     rendered_outputs = [env.render()]
-    total_reward = 0
+    total_rewards = 0
 
     for _ in range(max_steps):
-        # Exploit the Q-table (Greedy policy)
         action = np.argmax(Q[state])
-
-        # Apply the action
         state, reward, terminated, truncated, _ = env.step(action)
 
-        # Capture the current board state as a string
         rendered_outputs.append(env.render())
+        total_rewards += reward
 
-        total_reward += reward
-
-        # End episode if agent reached goal, fell in hole, or timed out
         if terminated or truncated:
             break
 
-    return total_reward, rendered_outputs
+    return total_rewards, rendered_outputs
